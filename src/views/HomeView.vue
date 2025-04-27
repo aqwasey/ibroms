185ac078890ee59a84e27411166e0528297107a3
SadickYahaya <sadickashton@live.com>
2025-04-27 10:11:24 +0000

    tailwind configuration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="text-4xl">Hello</h1>
 </template>
