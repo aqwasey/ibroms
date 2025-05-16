53d1da6ba61692828fe4b5d8759924533718d9ef
amosappiahnkum <amos.nkum@gmail.com>
2025-05-16 09:02:43 +0000

    add modal component add ant-design library

---
+++ src/components/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="show"
+       class="fixed inset-0 z-50 flex items-center justify-center bg-[#0000006b] p-5">
+    <div class="bg-white rounded-2xl shadow-lg w-full max-w-md p-6">
+      <div class="flex items-center justify-between">
+        <h2 v-if="title" class="text-lg text-i-black-soft font-semibold mb-4">{{ title }}</h2>
+        <button @click="close"
+                class="text-gray-500 hover:text-gray-700 text-2xl cursor-pointer">
+          &times;
+        </button>
+      </div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  show: Boolean,
+  close: Function,
+  title: {
+    type: String,
+    default: '',
+  },
+})
+</script>
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +15,13 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
     tailwindcss(),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false, // css in js
+        }),
+      ],
+    }),
   ],
   resolve: {
     alias: {
